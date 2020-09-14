219e07560dbc58ac3d336ccc9237dbbd616af227
unknown <gguranic@umass.edu>
2020-09-14 15:06:18 -0400

    global changes

--- 5_translate_df_to_jagslist_onecountry.R
+++ 5_translate_df_to_jagslist_onecountry.R
@@ -11,13 +11,13 @@ translate_data_to_jagslist <- function(
   ## warning: validationinfo is not yet fully tested in this version of the code!!
   validationinfo = list(RunValidation = FALSE, ValidateByTime = FALSE, cutoffyear = 2008, setseed = 0123)
 ){
-
+  
   datall <- main_data
-
+  
   ## warning: validationinfo is not yet fully tested in this version of the code!!
   if (validationinfo$RunValidation){
     if(validationinfo$ValidateByTime==TRUE){
-        datall$validate.exclude<-datall$year >= validationinfo$cutoffyear
+      datall$validate.exclude<-datall$year >= validationinfo$cutoffyear
     } else {
       set.seed(validationinfo$setseed)
       datall$validate.exclude<-rbinom(n=nrow(datall),1,prob=0.20)
@@ -25,34 +25,34 @@ translate_data_to_jagslist <- function(
   } else {
     datall$validate.exclude <- rep(FALSE, dim(datall)[1])
   }
-
+  
   #-------------------------------------
   set.seed(123456)
   logbeta <- rnorm(1000000, 0, 1)
-
+  
   #--------------
   jagsdata <- list()
   jagsdata$referenceyear <- referenceyear
   jagsdata$imputeSElogPM <- imputeSElogPM
-
+  
   #-------------------------------------
   # 1. model par and baselist
-
+  
   ## Time series parameters
   jagsdata$sqrtgamma0max <- sqrtgamma0max
   jagsdata$max.sigma.lambda <- max.sigma.lambda
-
+  
   ##aids parameters
   jagsdata$caids = meta$caids
   jagsdata$uaids = meta$uaids
   jagsdata$kaids = meta$kaids
-
+  
   jagsdata$year.ref<-which(meta$year.t==referenceyear)
-
+  
   X.cth <- meta$X.cth
   B.ct <- meta$births.ct
   isssa.c <- ifelse(meta$mdg.c =="Sub-Saharan Africa",1,0)
-
+  
   baselist <- list(
     X.cth = X.cth,
     isssa.c=isssa.c,
@@ -65,19 +65,19 @@ translate_data_to_jagslist <- function(
     Raids.ct = meta$v.ct*meta$uaids,
     muaids.ct = meta$v.ct*meta$uaids/B.ct*meta$a.ct*meta$deaths.ct
   )
-
+  
   #---------------
   # specialized studies with reported envelope, index is jinq
   isjinq_all.d <- (datall$type=="inq"
-               & !is.na(datall$final_pm)
-               &!is.na(datall$final_env)
-               &datall$modelinclude)
+                   & !is.na(datall$final_pm)
+                   &!is.na(datall$final_env)
+                   &datall$modelinclude)
   isjinq.d <- isjinq_all.d & datall$completeness_inq > 0.95
   isjinq_incomplete.d <- isjinq_all.d & !(datall$completeness_inq > 0.95)
-
+  
   datainq <- datall[isjinq.d,]
   datainq_incomplete <- datall[isjinq_incomplete.d,]
-
+  
   J <- dim(datainq)[1]
   getc.j = Getc.i(iso.i = paste(datainq$iso), iso.c = meta$iso.c)
   gett.j = Gett.i(years.i = floor(datainq$year), year.t = meta$year.t)
@@ -92,7 +92,7 @@ translate_data_to_jagslist <- function(
     partialtime.xj[1:X.j[j],j] <- GetPartialTime(start = start.j[j], end = end.j[j], X = X.j[j])
     partialwhoenv.xj[1:X.j[j],j] <- partialtime.xj[1:X.j[j],j]*meta$deaths.ct[getc.j[j], gettstart.j[j]:gettend.j[j]]
   }
-
+  
   datinq <-  list(
     isjinq.d = isjinq.d,
     Jinq = J,
@@ -105,11 +105,11 @@ translate_data_to_jagslist <- function(
     partialtime.xjinq  = partialtime.xj,
     partialwhoenv.xjinq  = partialwhoenv.xj
   )
-
+  
   # repeated coding, change outcome vars only in lsit that gets's outputted
   datainq <- datainq_incomplete
-
-
+  
+  
   J <- dim(datainq)[1]
   getc.j = Getc.i(iso.i = paste(datainq$iso), iso.c = meta$iso.c)
   gett.j = Gett.i(years.i = floor(datainq$year), year.t = meta$year.t)
@@ -124,16 +124,16 @@ translate_data_to_jagslist <- function(
     partialtime.xj[1:X.j[j],j] <- GetPartialTime(start = start.j[j], end = end.j[j], X = X.j[j])
     partialwhoenv.xj[1:X.j[j],j] <- partialtime.xj[1:X.j[j],j]*meta$deaths.ct[getc.j[j], gettstart.j[j]:gettend.j[j]]
   }
-
+  
   # add completeness
   varfrombeta.j <- rep(NA, J)
   for (j in 1:J){
     varfrombeta.j[j] <- var(1/(datainq$completeness_inq[j] +
                                  (1-datainq$completeness_inq[j])*exp(logbeta)))
   }
-
-
-
+  
+  
+  
   datinq2 <-  list(
     isjinq_incomplete.d = isjinq.d,
     Jinq_incomplete = J,
@@ -152,20 +152,20 @@ translate_data_to_jagslist <- function(
   # just reset to not cause problems later
   datainq <- datall[isjinq.d,]
   datainq_incomplete <- datall[isjinq_incomplete.d,]
-
+  
   #-------------------------------------
   # for VR data, excl vr-like
-
+  
   isj.d<-datall$type=="vr" & datall$modelinclude
   datavr <- datall[isj.d,]
-
+  
   getj.k1 <- seq(1, dim(datavr)[1])
   K1 <- length(getj.k1)
   datvrmultiplier <- list(
-      getj.k1 = getj.k1,
-      K1 = K1
+    getj.k1 = getj.k1,
+    K1 = K1
   )
-
+  
   varfrombeta <- rep(NA, dim(datavr)[1])
   for (j in 1:dim(datavr)[1]){
     varfrombeta[j] <- var(1/(datavr$rhovrfinal_bmat[j] + (1-datavr$rhovrfinal_bmat[j])*exp(logbeta)))
@@ -193,9 +193,9 @@ translate_data_to_jagslist <- function(
     input0forVR.ct = matrix(0, meta$C, meta$nyears),
     varfrombeta.j = varfrombeta
   )
-
-
-
+  
+  
+  
   #------------------------------------------------------------------------
   # other studies, index is jnew
   isjnew.d<- isj.d==FALSE & isjinq.d==FALSE & !is.na(datall$final_pm) & datall$modelinclude
@@ -219,15 +219,15 @@ translate_data_to_jagslist <- function(
   }
   selogpm.jnew <- datanonvr$obs_selogpm
   selogpm.jnew[is.na(selogpm.jnew)] <- imputeSElogPM
-
+  
   # data types are misc, with subtype dhs
   ismisc.jnew  <- datanonvr$type!="inq"
   isinq.jnew <- datanonvr$type=="inq"
   isdhs.jnew <- datanonvr$type=="dhs"
-
+  
   getjnew.m <- which(ismisc.jnew)
   M <- length(getjnew.m)
-
+  
   datnonvr <-  list(
     ismisc.jnew = ismisc.jnew,
     isinq.jnew = isinq.jnew,
@@ -245,7 +245,7 @@ translate_data_to_jagslist <- function(
     partialtime.xjnew  = partialtime.xj,
     partialwhoenv.xjnew  = partialwhoenv.xj,
     aidsva.jnew = aids.jnew)
-
+  
   #-------------------------------------
   if (validationinfo$RunValidation){
     getj.g = which(datavr$validate.exclude==TRUE)
@@ -257,43 +257,43 @@ translate_data_to_jagslist <- function(
     getjinq_incomplete.ginq = 1:datinq2$Jinq_incomplete
     getjnew.gnew = 1:datnonvr$Jnew
   }
-
+  
   datvalidation=list(
     Gtrain = datvr$J-sum(datavr$validate.exclude),
     Ginqtrain = datinq$Jinq-sum(datainq$validate.exclude),
     #to change for validation
     Ginqtrain_incomplete = datinq2$Jinq_incomplete - sum(datainq_incomplete$validate.exclude),
     Gnewtrain = datnonvr$Jnew-sum(datanonvr$validate.exclude),
-
+    
     getj.gtrain = which(datavr$validate.exclude==FALSE),
     getjinq.ginqtrain = which(datainq$validate.exclude==FALSE),
     getjinq_incomplete.ginqtrain = which(datainq_incomplete$validate.exclude==FALSE),
     getjnew.gnewtrain = which(datanonvr$validate.exclude==FALSE),
-
+    
     G= ifelse(validationinfo$RunValidation, sum(datavr$validate.exclude), datvr$J),
     Ginq= ifelse(validationinfo$RunValidation, sum(datainq$validate.exclude), datinq$Jinq),
     Ginq_incomplete = ifelse(validationinfo$RunValidation, sum(datainq_incomplete$validate.exclude), datinq2$Jinq_incomplete),
     Gnew=ifelse(validationinfo$RunValidation, sum(datanonvr$validate.exclude),datnonvr$Jnew),
-
+    
     getj.g = getj.g,
     getjinq.ginq = getjinq.ginq,
     getjinq_incomplete.ginq = getjinq_incomplete.ginq,
     getjnew.gnew = getjnew.gnew
-
+    
   )
-
+  
   #-------------------------------------
   jagsdata <- c(jagsdata, baselist, datvr, datinq,datinq2,
                 datnonvr
-                 , datvrmultiplier
+                , datvrmultiplier
                 ,datvalidation
   )
-
+  
   # for constraints
   jagsdata$input1.ct <- matrix(1, meta$C, meta$nyears)
   jagsdata$input1again.ct <- jagsdata$input1.ct
   #-------------------------------------
-
+  
   if (!dir.exists(here::here("output"))) dir.create(here::here("output"))
   if (!dir.exists(here::here("output", runname))) dir.create(here::here("output", runname))
   main_path <- file.path("output", runname)
